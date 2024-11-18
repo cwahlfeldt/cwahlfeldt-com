75114865f1c4bc0aee4e55f586eafbdbadc3a7dd
cwahlfeldt <chriswahlfeldt@gmail.com>
2024-11-18 12:47:27 -0600

    working!

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -25,3 +25,9 @@ if (window.netlifyIdentity) {
     }
   });
 }
+document.body.addEventListener("pointermove", (e) => {
+  const { currentTarget: el, clientX: x, clientY: y } = e;
+  const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+  el.style.setProperty('--posX', x - l - w / 2);
+  el.style.setProperty('--posY', y - t - h / 2);
+})
